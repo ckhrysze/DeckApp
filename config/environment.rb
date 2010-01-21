9bc50d1fa182c5d076f01c4d12e71a12e269ce04
ckhrysze <ckhrysze@gmail.com>
2010-01-20 22:20:08 -0600

    moving mongo setup into its own initializer

--- config/environment.rb
+++ config/environment.rb
@@ -11,37 +11,3 @@ Rails::Initializer.run do |config|
   config.gem 'mongo_mapper', :version => '>= 0.6.10'
   config.frameworks -= [:active_record]
 end
-
-db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
- 
-if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
-  Rails.logger.info("Setting up mongo connection")
-  mongo = db_config[Rails.env]
-
-  Rails.logger.info("Attempting to log into #{mongo['host']} port #{mongo['port']}")
-
-  MongoMapper.connection = Mongo::Connection.new(mongo['host'],
-                                                 mongo['port'],
-                                                 { :logger => Rails.logger })
-  
-  MongoMapper.database = mongo['database']
-  if mongo['username'].present?
-    Rails.logger.info("Attempting to pull credentials from env")
-    Rails.logger.info("Env says user is #{ENV['MONGOHQ_USER']}")
-    success = MongoMapper.database.authenticate(ENV['MONGOHQ_USER'], ENV['MONGOHQ_PASS'])
-
-    Rails.logger.info("auth call was successful? #{success}")
-
-    begin
-      raise "Mongo auth failed" unless success
-    rescue => e
-      Rails.logger.info("auth failed, tell hoptoad about it")
-      var_hash = {:params => mongo}.merge(:user => ENV['MONGOHQ_USER'])
-      result = HoptoadNotifier.notify(:error_class   => "MongoHQ connection failure",
-                                      :error_message => "MongoHQ connection failure: #{e.message}",
-                                      :request       => var_hash
-                                      )
-    end
-  end
-end
-
