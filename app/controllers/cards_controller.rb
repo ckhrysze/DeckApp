dfdc742946fe1798644b9f275b8130137e6c4b71
ckhrysze <ckhrysze@gmail.com>
2010-01-20 22:37:04 -0600

    hoping for better errors if I try to connent to mongo later

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,15 +8,24 @@ class CardsController < ApplicationController
     #  format.html # index.html.erb
     #  format.xml  { render :xml => @cards }
     #end
-    begin
-      raise "DOOM"
-    rescue => e
-      var_hash = {:params => {:a => 1, :b => 2}}
-      Rails.logger.info("about to notify")
-      HoptoadNotifier.notify(:error_class   => "MongoHQ connection failure",
-                             :error_message => "MongoHQ connection failure: #{e.message}",
-                             :request       => var_hash
-                             )
+    db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
+
+    Rails.logger.info("Setting up mongo connection")
+    mongo = db_config[Rails.env]
+
+    Rails.logger.info("Attempting to log into #{mongo['host']} port #{mongo['port']}")
+    
+    MongoMapper.connection = Mongo::Connection.new(mongo['host'],
+                                                   mongo['port'],
+                                                   { :logger => Rails.logger })
+    MongoMapper.database = mongo['database']
+
+    if mongo['username'].present?
+      Rails.logger.warn("Attempting to pull credentials from env")
+      Rails.logger.warn("Env says user is #{ENV['MONGOHQ_USER']}")
+      success = MongoMapper.database.authenticate(ENV['MONGOHQ_USER'], ENV['MONGOHQ_PASS'])
+
+      Rails.logger.warn("auth call was successful? #{success}")
     end
   end
 
